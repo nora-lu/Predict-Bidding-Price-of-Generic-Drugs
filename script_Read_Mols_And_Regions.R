00741b5eb3cfd07f1b2d74c6f836151cfdd54ffb
chemsslu <chem.sslv@gmail.com>
2016-01-10 19:23:20 -0500

    Divide molecules data by year

--- script_Read_Mols_And_Regions.R
+++ script_Read_Mols_And_Regions.R
@@ -5,6 +5,13 @@ molecules <- read.csv("CAX_AdditionalData/CAX_AdditionalData_Molecules.csv",
 
 regions <- read.csv("CAX_AdditionalData/CAX_AdditionalData_Regions.csv",
                     header = TRUE)
+
+trainMol345 <- read.csv("CAX_Bidding_TRAIN_Molecule_3_4_5.csv")
+
+testMol6Pre <- read.csv("CAX_Bidding_TEST_Molecule_6_Pre_LOE.csv")
+
+testMol6Post <- read.csv("CAX_Bidding_TEST_Molecule_6_Post_LOE.csv")
+
 columnsAAndB<-(regions[,c(2:15)])
 
 for (i in names(columnsAAndB)) {
